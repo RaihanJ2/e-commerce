c826e30d5b539a6f18c3f45b8cc0548964af52b1
Raihanjansmaillendra <raihanjansmaillendra.rjs@gmail.com>
2025-05-06 06:17:36 +0700

    font fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,9 @@ import Nav from "@components/Nav";
 import "@styles/globals.css";
 import Provider from "@components/Provider";
 import React from "react";
+import { Didact_Gothic } from "next/font/google";
 
+const Gothic = Didact_Gothic({ weight: "400", subsets: ["latin"] });
 export const metadata = {
   title: "Baby Store",
   description: "An e-commerce site for baby products",
@@ -11,7 +13,10 @@ export const metadata = {
 
 const RootLayout = ({ children }) => (
   <html lang="en">
-    <body className="font-sans min-h-screen" suppressHydrationWarning={true}>
+    <body
+      className={`${Gothic.className} font-sans min-h-screen`}
+      suppressHydrationWarning={true}
+    >
       <Provider>
         <div className="main bg-primary-darkest"></div>
         <main className="app bg-primary-darkest flex flex-col min-h-screen">
